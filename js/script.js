5046440742ed07015dd285ca5efc651ea3428977
Caleb Pierce <caleb@insidenewcity.com>
2012-10-29 11:42:55 -0400

    comments to help David with implementation

--- js/script.js
+++ js/script.js
@@ -9,8 +9,19 @@
     _signIn: function() {
       return signup_form.$form.submit(function(e) {
         var username;
-        e.preventDefault();
+        /*
+              DAVID DO YOUR SIGN IN MAGICZ HERE!!!1!1
+        */
+
+        
+        /*
+              DAVID SET THE USERNAME HERE
+        */
         username = $(this).find('#username').val();
+        /*
+              DAVID PUT THIS IN YOUR CALLBACK:
+        */
+
         $('#chat-wrap').fadeIn('fast', function() {
           return $(this).find('.message-input').focus();
         });
@@ -30,13 +41,29 @@
       return chat.$compositionForm.submit(function(e) {
         var message, return_data;
         e.preventDefault();
+
+        /*
+              DAVID THE MESSAGE TO SEND IS STORED IN THE "message" VARIABLE. DUH!
+        */
         message = $(this).find('.message-input').val() || $(this).find('.message-input').text();
+
         $(this).find('.message-input').text('').val('');
+        
+        /*
+              DAVID SEND ME BACK AN OBJECT WITH THESE VALUES. MAY NEED MORE LATER
+        */
         return_data = {
           timestamp: 'hh:mm',
           username: 'Username',
           message: message
+          /*
+                  DAVID SET return_data.message TO THE CHAT MESSAGE THE SERVER SENT TO THE USER
+          */
+
         };
+        /*
+              DAVID THE FOLLOWING IS THE CALLBACK FOR A SUCCESFULLY SENT MESSAGE
+        */
         chat.$chatWindow.find('.messages').append('<li class="sent"><span class="timestamp">' + return_data.timestamp + '</span> ' + return_data.message + ' <span class="username username-placeholder">' + return_data.username + '</span></li>');
         return chat.$chatWindow.animate({
           scrollTop: chat.$chatWindow.height()
