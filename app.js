2555a4bfe16e43b549412a7156614f6fc3580e60
David Overcash <funnylookinhat@gmail.com>
2012-11-01 10:13:14 -0600

    Initial code for server side of things - should function, but is missing some organization and caching features.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Includes
+var sanitize = require('./includes/sanitize');
+
 // Basic HTTP Delivery with Express
 var express = require('express');
 var app = express();
@@ -6,6 +9,22 @@ var server = require('http').createServer(app);
 // Database w/ Redis
 var redis = require('redis');
 var db = redis.createClient();
+
+// Pick a number... any number...  0-15.
+db.select(13);
+
+// We prefix all of our keys with this to avoid any collision.
+var DB_PRE = "nodejs-chat-";
+// And some key helpers.  No strings in our lines!
+var DB_USERNAME_PRE = "username-";
+var DB_MESSAGES = "messages";
+
+// Do some quick database cleanup.
+db.del(DB_PRE+DB_MESSAGES,function(error,value) {
+	if( error ) {
+		console.log("Error removing previous messages: "+error);
+	}
+});
 
 // Socket.IO
 var io = require('socket.io').listen(server);
@@ -27,3 +46,104 @@ app.get('/js/*', function(req, res) {
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
+
+// TODO - Restructure this to handle entire event.
+function addMessage(socket,username,text) {
+	var eventTime = String(Math.round(new Date().getTime() / 1000));
+	// Add database caching here.
+	socket.broadcastEmit('message',{
+		nick: username,
+		text: text,
+		time: eventTime
+	});
+}
+
+// Socket Listeners and Events
+io.sockets.on('connection', function (socket) {
+  
+  // 'nick' Event
+  // Change or set a nickname.
+  socket.on('nick', function(data) {
+  	var eventTime = String(Math.round(new Date().getTime() / 1000));
+  	var oldUsername = null;
+  	db.get(DB_PRE+DB_USERNAME_PRE+socket.id,function(error,value) {
+  		if( value != null ) {
+  			oldUsername = value;
+  		}
+  	});
+  	db.set(DB_PRE+DB_USERNAME_PRE+socket.id,data.nick);
+  	if( oldUsername == null ) {
+  		// New user joined chat.
+  		socket.broadcastEmit('event',{
+  			text: data.nick+' has joined the room.',
+  			type: 'neutral',
+  			time: eventTime
+  		});
+  		// If you want to have a user get the last 10 messages or so...
+  		// This would be the place to do it.
+  	} else {
+  		// User changed nick.
+  		socket.broadcastEmit('event',{
+  			text: oldUsername+' has changed their name to '+data.nick+'.',
+  			type: 'neutral',
+  			time: eventTime
+  		});
+  	}
+  });
+
+  // 'message' Event
+  // Send a message.
+  socket.on('message', function(data) {
+  	var eventTime = String(Math.round(new Date().getTime() / 1000));
+  	if( data.text == undefined || 
+  		! data.text.length ) {
+  		socket.emit('event',{
+			text: 'That message did not include a text element.',
+			type: 'error',
+			time: eventTime
+		});
+  		return;
+  	}
+  	// Validate user has set a username.
+  	db.get(DB_PRE+DB_USERNAME_PRE+socket.id,function(error,value) {
+  		var username = value;
+  		if( username == null ) {
+  			socket.emit('event',{
+  				text: 'You must set a nickname before you can send messages.',
+  				type: 'error',
+  				time: eventTime
+  			});
+  		} else {
+  			addMessage(socket,username,data.text);
+  		}
+  	});
+  });
+
+  socket.on('get', function(data) {
+  	db.get(socket.id,function(err,value) {
+  		if( err ) {
+  			socket.emit('getResponse', {status:"ERROR: "+err.toString()});
+  		} else {
+  			socket.emit('getResponse', {status:value});
+  		}
+  	});
+  });
+  socket.on('set', function(data) {
+  	db.set(socket.id,socket.id+':'+data.status);
+  	socket.emit('getResponse', {status:socket.id+':'+data.status});
+  });
+});
+
+/*
+db.get('keythatdoesnotexists',function(error,value) {
+	console.log("ERROR");
+	console.log(error);
+	console.log("VALUE");
+	console.log(value);
+	if( value == null ) {
+		console.log("NULL!");
+	} else {
+		console.log("Not null?");
+	}
+});
+*/
