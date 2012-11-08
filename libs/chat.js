e9a9013f06955dcd740f324319f749a463b3f834
David Overcash <funnylookinhat@gmail.com>
2012-11-08 09:06:21 -0700

    Added disconnect event for users.

--- libs/chat.js
+++ libs/chat.js
@@ -97,6 +97,26 @@ exports = module.exports = function(params) {
         }
       }
     });
+
+    // User disconnects from chatroom.
+    socket.on('disconnect', function () {
+      var nick = String(nicks[socket.id]);
+      nicks.splice(socket.id,1);
+
+      log.unshift({
+        event: 'notification',
+        packet: packets.Notification({
+          nick: nick,
+          type: 'neutral',
+          text: ' has left the room.'
+        })
+      });
+
+      socket.broadcast.emit(log[0].event,log[0].packet);
+      if( log.length > 10 ) {
+        log.pop();
+      }
+    });
   });
 };
 
